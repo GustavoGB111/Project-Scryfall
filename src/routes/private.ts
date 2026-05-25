a54ee158d56afb4469b3979fecea63cfa00aac65
GustavoGB111 <gbgustavo172@gmail.com>
2026-05-25 16:46:42 -0300

    parte 1 finalizada com sucesso

--- src/routes/private.ts
+++ src/routes/private.ts
@@ -1,1 +1,16 @@
-import express from 'express'
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/listar-usuarios', async (req, res) => {
+    try {
+        // buscar a lista de usuarios no Database
+        const user = 0;
+
+        res.status(200).json({message: "Lista de usuarios", user})
+    } catch (error) {
+        res.status(500).json({message: "Falha no servidor"});
+    }
+});
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import "dotenv/config";
 import express from 'express';
 import publicRoutes from './routes/public.js';
+import privateRoutes from './routes/private.js';
+import auth from "./middlewares/auth.js";
 import { AppDataSource } from './DB/databaseConexion.js';
 
 const app = express();
@@ -11,6 +13,7 @@ app.use(express.json());
 app.use('/user', publicRoutes);
 
 //Rotas privadas
+app.use('/user', auth, privateRoutes); // passa primeiro pelo auth para acessar as rotas privadas
 
 
 //inicializando o banco de dados
